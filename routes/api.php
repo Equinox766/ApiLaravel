12dbfcd5655d5c8c970f8c2e603fcae8a3b9aac4
EmanuelLezcano <emanuel.lezcano@biggie.com.py>
2023-04-11 16:38:27 -0400

    laravel api

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+//Route::resource('products', ProductController::class);
+
+//Rutas publicas
+Route::post('/register',[AuthController::class, 'register']);
+Route::post('/login',[AuthController::class, 'login']);
+Route::get('/products',[ProductController::class, 'index']);
+Route::get('/products/{$id}',[ProductController::class, 'show']);
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
+
+
+//Rutas protegidas
+Route::group(['middleware' => ['auth:sanctum']], function () { 
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{$id}',[ProductController::class, 'update']);
+    Route::delete('/products/{$id}',[ProductController::class, 'destroy']);
+    Route::post('/logout',[AuthController::class, 'logout']);
+});
+ 
+
+ 
